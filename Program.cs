e33e4e44697ab0c150974bde8138b10d0507d0c1
sebastian <sebastiandev.img@gmail.com>
2025-10-06 23:38:25 -0300

    fix swagger

--- Program.cs
+++ Program.cs
@@ -17,12 +17,12 @@ builder.Services.AddTransient<IEmailService, EmailService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    options.RoutePrefix = string.Empty; // so Swagger is served at root "/"
+});
 
 app.UseHttpsRedirection();
 
